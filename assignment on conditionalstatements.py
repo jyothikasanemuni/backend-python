5bc903941ea9a43d9a811c300e6e97da1bb95ff0
jyothika <jyothikasanemuni@gmail.com>
2025-08-14 01:15:37 +0530

    Added assignment on conditionalstatements.py

---
+++ assignment on conditionalstatements.py
@@ -0,0 +1,47 @@
+#check if a number is divisible by both 3 and 5
+"""num=int(input("enter a number:"))
+if num%3==0 and num%5==0:
+    print("The num is divisible by both 3 and 5")
+elif num%3==0:
+    print("the number is divisible by 3")
+elif num%5==0:
+    print("the num id  divisible by 5")
+else:
+    print("not divisible")"""
+
+# compare two numbers and print which one is greater
+"""a=int(input("first num:"))
+b=int(input("second num:"))
+if  a>b:
+    print("the first number is greater than the second num")
+elif  b>a:
+    print("the second num is greater than the first num")
+else:
+    print("Both nums are equal")"""
+
+#ask for a score and print grade A/B/C/FAIL
+"""score=int(input("enter your score:"))
+if score>=90:
+    print("Grade A")
+elif score>=50:
+    print("Grade B")
+elif score>=35:
+    print("Grade C")
+else:
+    print("fail")"""
+#check if a number is positive and even
+"""num=int(input("enter a num:"))
+if num%2==0 and num>0:
+    print("the num is postive and even")
+elif num<0:
+    print("the num is negative")
+else:
+    print("the number is odd")"""
+#ask the user age and suggest child if<13 teen if 13-19 adult if 20+
+age=int(input("enter your age:"))
+if age <13:
+    print("you're a child")
+elif 13<=age<=19:
+    print("you're an teenager")
+else:
+    print("you're an adult")
